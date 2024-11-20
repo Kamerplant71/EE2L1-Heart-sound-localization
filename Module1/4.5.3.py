7995f73462d6e93c064aa500ca9d7db7e5c92299
Rein <rlvandervelden@tudelft.nl>
2024-11-20 17:23:44 +0100

    laatste commit(filter werkt miss niet?)

--- Module1/4.5.3.py
+++ Module1/4.5.3.py
@@ -9,13 +9,14 @@ from scipy.fft import fft,ifft
 
 Fs, x = wavfile.read("Module1\heart_single_channel_physionet_49829_TV.wav")
 period = 1/Fs
-xn= x/max(x)
+xn= x/max(abs(x))
 xi = xn**2
 xi = xi + 1e-10
 E=-(xi *np.log10(xi))
 
 b, a = butter(2, [15/2000], btype='low')
 y = signal.lfilter(b,a,E)
+y = y.real
 Y= fft(y)
 t= np.linspace(0,period*len(y),len(y))
 f= np.linspace(0,Fs, len(Y))
@@ -27,7 +28,7 @@ plt.title("Time plot filtered E")
 plt.tight_layout()
 
 plt.subplot(212)
-plt.plot(f,Y)
+plt.plot(f,abs(Y))
 plt.xlabel("Frequency")
 plt.ylabel("Magnitude")
 plt.title("Frequency filtered E")
