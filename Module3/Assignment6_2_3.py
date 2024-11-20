5a544206aed8e97259a06f06e8cc655edf43f66e
Wessel Oosterkamp <woosterkamp@tuddelft.nl>
2024-11-20 16:12:41 +0100

    6.4

--- Module3/Assignment6_2_3.py
+++ Module3/Assignment6_2_3.py
@@ -2,12 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def a_lin(theta, M, d, v, f0):
-    a = np.transpose(np.ones((M),dtype="complex_"))
+    a = np.reshape(np.ones((M),dtype="complex_"),(M,1))
 
-    for i in range(1,M+1):
-        a[i-1] = a[i-1] * np.exp(-1j*(i-1)*d/v*np.sin(theta)*2*np.pi*f0)
+    for i in range(0,M):
+        a[i] = a[i] * np.exp(-1j*(i)*d/v*np.sin(theta)*2*np.pi*f0)
         
     return a
 
-#print((a_lin(np.pi/2,3,1,340,60)))
+print((a_lin(np.pi/2,3,1,340,60)))
 
