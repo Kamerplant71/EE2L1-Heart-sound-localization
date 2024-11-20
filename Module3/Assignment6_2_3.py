f56905466189149f74bbc8e686684acb712723fb
Justen van Koppen <j.vankoppen-2@student.tudelft.nl>
2024-11-20 16:14:53 +0100

    alin update

--- Module3/Assignment6_2_3.py
+++ Module3/Assignment6_2_3.py
@@ -2,12 +2,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def a_lin(theta, M, d, v, f0):
-    a = np.transpose(np.ones((M),dtype="complex_"))
-
-    for i in range(1,M+1):
-        a[i-1] = a[i-1] * np.exp(-1j*(i-1)*d/v*np.sin(theta)*2*np.pi*f0)
+    a = (np.ones((M,1),dtype="complex_"))
+    for i in range(0,M):
+        a[i] = a[i] * np.exp(-1j*(i)*d/v*np.sin(theta)*2*np.pi*f0)
         
     return a
 
-#print((a_lin(np.pi/2,3,1,340,60)))
+
+def a_lin_multiplesources(theta, M, d, v, f0):
+    a = np.ones((M,len(theta)),dtype="complex_")
+    for j in range(0,len(theta)):
+        for i in range(0,M):
+            a[i][j] = np.exp(-1j*(i)*d/v*np.sin(theta[j])*2*np.pi*f0)
+        
+    return a
+
+theta = [np.pi/2]
+
+print(a_lin(np.pi/2,3,1,340,60))
+print((a_lin_multiplesources(theta,3,1,340,60)))
 
