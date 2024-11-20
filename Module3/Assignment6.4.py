0f8ece048540cf201d9956b65964447b30b19217
Wessel Oosterkamp <woosterkamp@tuddelft.nl>
2024-11-20 20:48:48 +0100

    MVDR

--- Module3/Assignment6.4.py
+++ Module3/Assignment6.4.py
@@ -14,12 +14,32 @@ def matchedbeamformer(Rx, th_range, M, d, v, f0):
         a_theta= a_lin(th_range[i], M, d, v, f0)
         a_H_theta = np.reshape( a_theta.conj(), (1,M) )
         #Calculate Power
-        print(np.shape(a_H_theta))
-        print(np.shape(a_theta))
+       # print(np.shape(a_H_theta))
+        #print(np.shape(a_theta))
         py= abs(np.matmul(np.matmul(a_H_theta,Rx),a_theta))
         Py[i] = py[0][0]
 
     return Py
+
+def mvdr(Rx, th_range, M, d, v, f0):
+    Rxinv= np.linalg.inv(Rx)
+    
+    Py = np.zeros((len(th_range)),dtype="complex_")
+
+    #Loop for each theta
+    for i in range(0,len(th_range)):
+        #Calculate a_theta
+        a_theta= a_lin(th_range[i], M, d, v, f0)
+        a_H_theta = np.reshape( a_theta.conj(), (1,M) )
+        #Calculate Power
+      #  print(np.shape(a_H_theta))
+       # print(np.shape(a_theta))
+        py1= abs(np.matmul(np.matmul(a_H_theta,Rxinv),a_theta))
+        py= 1/py1
+        Py[i] = py[0][0]
+
+    return Py
+    
 
 #Initialize Constants
 theta0= [0,np.pi/12]
@@ -27,9 +47,10 @@ Q = len(theta0)
 SNR = 10
 th_range = np.linspace(-np.pi/2,np.pi/2, 100)
 M=7
-d= 1
+delta =0.5
 v= 340
-f0= 140
+f0= 300
+d = delta*v/f0
 
 #Calculate Rx
 sigma_n = 10**(-SNR/20); # std of the noise (SNR in dB)
@@ -41,9 +62,18 @@ Rx = R + Rn # received data covariance matrix
 print(np.shape(Rx))   
     
 P_mbf = matchedbeamformer(Rx, th_range, M, d, v, f0)
+P_mvdr = mvdr(Rx, th_range, M, d, v, f0)
+
 
 print(np.shape(P_mbf))
 plt.plot(th_range*180/np.pi,(P_mbf))
 plt.xlabel("Angle [deg]")
 plt.ylabel("Power ")
-plt.show()
+plt.show()   
+
+print(np.shape(P_mvdr))
+plt.plot(th_range*180/np.pi,(P_mvdr))
+plt.xlabel("Angle [deg]")
+plt.ylabel("Power ")
+plt.show()     
+
