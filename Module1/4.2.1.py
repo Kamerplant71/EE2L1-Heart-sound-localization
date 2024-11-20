8811fa27ff19f0c16d10c5996527cf0672ea35db
Rein <rlvandervelden@tudelft.nl>
2024-11-20 11:52:22 +0100

    4.2.1

--- Module1/4.2.1.py
+++ Module1/4.2.1.py
@@ -2,5 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 
+Fs, data = wavfile.read()
 b, a = butter(2, [10, 800], btype='band')
 filtered_data = signal.filtfilt(b, a, data)
