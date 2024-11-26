41bff24e54ef919e41a2bd797a9ae87d2100b2ab
Justen van Koppen <j.vankoppen-2@student.tudelft.nl>
2024-11-26 17:35:56 +0100

    Lijpe dingen

--- Module3/Functions.py
+++ Module3/Functions.py
@@ -28,9 +28,25 @@ def matchedbeamformer(Rx, th_range, M, d, v, f0):
         a_theta= a_lin(th_range[i], M, d, v, f0)
         a_H_theta = np.reshape( a_theta.conj(), (1,M) )
         #Calculate Power
-        print(np.shape(a_H_theta))
-        print(np.shape(a_theta))
         py= abs(np.matmul(np.matmul(a_H_theta,Rx),a_theta))
         Py[i] = py[0][0]
 
     return Py
+
+def mvdr(Rx, th_range, M, d, v, f0):
+    Py = np.zeros((len(th_range)),dtype="complex_")
+    Rxinv= np.linalg.inv(Rx)
+
+    #Loop for each theta
+    for i in range(0,len(th_range)):
+        #Calculate a_theta
+        a_theta= a_lin(th_range[i], M, d, v, f0)
+        a_H_theta = np.reshape( a_theta.conj(), (1,M) )
+
+        #Calculate Power
+        py1= (np.matmul(np.matmul(a_H_theta,Rxinv),a_theta))
+        py= 1/py1
+        Py[i] = py[0][0]
+
+    return Py
+    
