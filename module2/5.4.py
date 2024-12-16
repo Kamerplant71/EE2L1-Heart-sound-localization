0ec96d7c6ad9f1523819d58a1e1f09bdbefd0662
Rein <rlvandervelden@tudelft.nl>
2024-12-16 11:20:17 +0100

    skibidi4

---
+++ module2/5.4.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import butter, TransferFunction, lsim, tf2zpk
+from scipy import signal
+from scipy.io import wavfile
+from scipy.fft import fft,ifft
+
